3670c640c75d6e7b3c0c27f287622abb87b25961
Konstantin Misyutin <ikeeip@arello-mobile.com>
2016-09-03 17:29:27 +0700

    [artefact][WDT-000] update setup.py. add python 3.5 classifier.

--- setup.py
+++ setup.py
@@ -26,5 +26,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
